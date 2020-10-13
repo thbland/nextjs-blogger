bbdbc897596ffb22f011183ed4bbc13672cadd34
Troy Bland <troybland@gmail.com>
2020-10-13 14:35:49 -0500

    first comment

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@ const webpack = require('webpack');
 
 require('dotenv').config();
 module.exports = {
-  webpack: config => {
+  webpack: (config) => {
     config.plugins.push(
       new webpack.EnvironmentPlugin(['BLOGGER_URL', 'BLOGGER_API_KEY'])
     );
     return config;
-  }
+  },
 };
+// figure this out
